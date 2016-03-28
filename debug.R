432010480d9aea50724eed57d22d9d9877a75c15
Max Xiong <xiongzhaohui@MacBook-Pro.local>
2016-03-28 14:11:48 -0400

    Tosco function worked

--- debug.R
+++ debug.R
@@ -1,15 +1,14 @@
-toscore <- function(sentences){
-  n <- 0
-    for (i in 1: length(pos.list)){
-        n = n+ str_count(as.character(sentences),as.character(pos.list[[i]]))
-    }  
-    for (k in 1: length(neg.list)){
-        n = n-str_count(as.character(sentences),as.character(neg.list[[k]]))
-    }
-  return(n)
-}
+source("toscore.R")
+
+toscore("good")
+
+lapply(some_txt,toscore)
+
 scorelist <- lapply(some_txt,toscore)
 
+for (i in 1:length(scorelist)){
+  n = n+scorelist[[i]]
+}
 
 library(stringr)
   str_count("man drives man", "man")
